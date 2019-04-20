d962d820454163c1222d424d70b1df2304f1311f
SputCoder <42704536+SputCoder@users.noreply.github.com>
2019-04-20 17:00:08 +0100

    Last of the Class imports Projs next
    Add a bit more to this file during next week

---
+++ PyC9-Classes-last.py
@@ -0,0 +1,36 @@
+## Caibidil 9
+
+# print
+print('\n\n###\n\nCaibidil :\n\n')
+print('Classes...')
+
+### SEE FILE dog.py to see where the Dog Class is stored
+
+
+
+######### IMPORTING CLASSES
+# Have all Classes stored in another file and when some Class is needed for code just import it.
+
+#IF THE Dog CLASS WAS ON ANOTHER FILE WOULD JUST NEED TO PUT THE FOLLOWING ON THE NEW FILE PROGRAM
+
+from dog import Dog    
+# the Dog is the Class in the dog.py file
+# dog is a file called dog.py
+
+# an object is needed to me made or instantiated
+my_dog = Dog('Tory', 8)  # this is the instance and is the object that has attributes name and age
+
+# needed the values name (Tory) and age(8)
+# values can be anything... can have any name or any age
+
+# sit... my_dog.sit()   ... need print to show output to screen only
+print(my_dog.sit())
+
+
+
+# now roll over... my_dog.roll_over()
+print(my_dog.roll_over())
+
+
+
+print('\n\nScroll to top to view all outputs!')
